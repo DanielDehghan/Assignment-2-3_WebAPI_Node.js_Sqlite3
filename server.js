7432449a02a4fe196a2b47b18cc6f69d7f80ecb8
Danial Takdehghan <98669366+DanielDehghan@users.noreply.github.com>
2024-11-30 12:23:33 -0500

    Modifies route file location and vercel.json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import greetingRouter from './routes/greetings.js';
+import greetingRouter from './api/greetings.js';
 
 dotenv.config();
 
