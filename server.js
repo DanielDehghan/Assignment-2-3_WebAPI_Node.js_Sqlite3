1cb7fb5e19c1a424b1ea0c01d97fae280777755c
Danial Takdehghan <98669366+DanielDehghan@users.noreply.github.com>
2024-12-01 01:07:05 -0500

    Initial commit for Vercel deployment

--- server.js
+++ server.js
@@ -7,15 +7,17 @@ dotenv.config();
 
 const app = express();
 
+// Middleware
 app.use(express.json());
 app.use(cors());
 
+// Define routes
 app.use('/api/greetings', greetingRouter);
 
 app.get('/health', (req, res) => res.status(200).send('Server is healthy!'));
-
 app.get('/', (req, res) => res.send('Welcome to the API!'));
 
-export default app;
-
-
+// Export a function for Vercel's serverless platform
+export default (req, res) => {
+  app(req, res); // Pass the request to express app
+};
