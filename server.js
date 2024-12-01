5557250d981d30e639643e5b20ae1d19e0bb7f56
Danial Takdehghan <98669366+DanielDehghan@users.noreply.github.com>
2024-12-01 01:47:21 -0500

    Your commit message here

--- server.js
+++ server.js
@@ -17,13 +17,7 @@ app.use('/api/greetings', greetingRouter);
 app.get('/health', (req, res) => res.status(200).send('Server is healthy!'));
 app.get('/', (req, res) => res.send('Welcome to the API!'));
 
-// Check if we are running in a local environment
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3000;
-  app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-  });
-}
-
-// Export the app for Vercel's serverless platform
-export default app;
+// Export a function for Vercel's serverless platform
+export default (req, res) => {
+  app(req, res); // Pass the request to express app
+};
